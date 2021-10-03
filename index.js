67e9e06a3d6c37367b69ddbf51e685063df90f9f
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2021-10-03 13:04:07 +0300

    first Commit

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const Socketio = require('socket.io')
+const redisAdapter = require('socket.io-redis');
 const port = process.env.PORT || 3000;
+
+io = Socketio(http, {
+  cors: {
+    origins: ['*'],
+    methods: ['GET', 'POST'],
+  },
+  transports: ['websocket'],
+});
+
+io.adapter(redisAdapter({
+  host: 'redis',
+  port: 6379,
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+  socket.emit('welcome', { message: `User cluster pID = ${process.pid}` });
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
